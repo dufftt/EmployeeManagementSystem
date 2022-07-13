77d7940ffc444e8de9606d88bfab44ace34feac2
tusharchaudhari30 <chaudharitushar2077@gmail.com>
2022-07-13 12:23:24 +0530

    Initial commit 🚀 🚀

---
+++ employeservice/src/main/java/com/cts/employeservice/EmployeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cts.employeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeserviceApplication.class, args);
+    }
+
+}
---
+++ employeservice/src/test/java/com/cts/employeservice/EmployeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.employeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
